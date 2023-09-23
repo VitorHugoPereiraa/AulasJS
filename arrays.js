4d3d703f11c414c386c53c294c47133ecad6df05
Vitor Hugo Pereira <vitorhugopereiraa@outlook.com>
2023-09-23 14:46:41 -0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,39 @@
+//Pegar os users com inicial V ou L
+
+// map - filter - find retorna um novo array;
+
+// forEach
+
+//Filter
+// function getUsers() {
+//   let arrayFiltrado = users.filter((user) => {
+//     let eVouL = user[0] === "Z" || user[0] === "B";
+//     return eVouL;
+//   });
+// }
+// getUsers();
+
+//Map
+
+// let novoArray = users.map((user) => {
+//   const [nome, sobrenome] = user.split(" ");
+//   return nome[0] + sobrenome[0];
+// });
+// console.log(novoArray);
+// const users = ["Vitor Hugo", "Cristiano Teste", "Lucas Sla", "Ana Julia"];
+
+// let newArray = users.map((user) => {
+//   return `<p>${user}</p>`;
+// });
+// console.log(newArray);
+
+// const users = [
+//   { name: "Vitor", email: "teste" },
+//   { name: "Adriano", deleted: true },
+// ];
+
+// // == ===
+// const newArray = users.filter((user) => {
+//   return ;
+// });
+// console.log("newArray", newArray);
---
+++ variaveis.js
@@ -0,0 +1,21 @@
+// TIPOS EXISTENTES
+
+//CONST
+
+// const sqlConnection = "sql:user=12313;password=12313;";
+// console.log(sqlConnection);
+//VAR
+
+// function teste() {
+//   var result = "teste";
+//   console.log(result);
+// }
+// teste();
+//LET
+
+// LET é uma variavel que se pode ter alterações
+// let nome = "Cristiano";
+
+// nome = [];
+
+// console.log(nome);
